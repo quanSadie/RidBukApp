049d427bc3c480b1372b03824b8761810441464a
Lê Anh Quân <quanrealvn@gmail.com>
2022-12-07 20:44:40 +0700

    sa

--- src/main/java/DBConnect/DBInfo.java
+++ src/main/java/DBConnect/DBInfo.java
@@ -1,10 +1,9 @@
 package DBConnect;
 
-// For security and my privacy, I will not provide database credentials here, you can create your own database
 public interface DBInfo {
 	public static String driverName = "org.postgresql.Driver";
 	public static String url = "jdbc:postgresql://"
-			+ "ec2-44-209-57-4.compute-1.amazonaws.com:5432/de7o4n2v5uc074?sslmode=require";
-	public static String user = "ssehzzhfgdqxco";
-	public static String pass = "faa289c438352b202945a781a1a266e04e95724ae85288a1a5841cdcc8339175";
+			+ "quotationdb-do-user-12815124-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require";
+	public static String user = "doadmin";
+	public static String pass = "AVNS_WKrO6g1S-oxJ1PnE4L7";
 }
--- src/main/webapp/staff.jsp
+++ src/main/webapp/staff.jsp
@@ -61,6 +61,8 @@
         </div>
 
 
+        <a class="nav-link" href="addbook.jsp">Add a book</a>
+
     </nav>
     <!-- Header & navigation bar END-->
 
